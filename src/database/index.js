a44ccf5972f4a476d223c5a47fe805d5b3571733
Joaquim Alves <jafn92@gmail.com>
2019-04-26 11:31:42 -0300

    Update index.js
    alter connection database

--- src/database/index.js
+++ src/database/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 //mongodb+srv://admin:<password>@cluster0-byjrf.mongodb.net/test?retryWrites=true
 
 //mongoose.connect('mongodb://admin:admin@cluster0-byjrf.mongodb.net/backend?retryWrites=true', { useNewUrlParser: true });
-mongoose.connect('mongodb+srv://admin:admin@cluster0-byjrf.mongodb.net/test?retryWrites=true', { useNewUrlParser: true });
+mongoose.connect('mongodb://admin:admin@cluster0-byjrf.mongodb.net/backend?retryWrites=true', { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;

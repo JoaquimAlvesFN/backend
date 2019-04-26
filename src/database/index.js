80e4496081f5d1271ef33566df89d759e6dbecf2
Joaquim Alves <jafn92@gmail.com>
2019-04-25 23:04:35 -0300

    alteracao de conexao do DB

--- src/database/index.js
+++ src/database/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 //mongodb+srv://admin:<password>@cluster0-byjrf.mongodb.net/test?retryWrites=true
 
-mongoose.connect('mongodb://admin:<password>@cluster0-byjrf.mongodb.net/Cluster0?retryWrites=true', { useNewUrlParser: true });
+mongoose.connect('mongodb://admin:admin@cluster0-byjrf.mongodb.net/Cluster0?retryWrites=true', { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;

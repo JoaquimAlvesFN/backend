71359bb24bee6f2babff713530b22fc1d312eee2
Joaquim Alves <jafn92@gmail.com>
2019-04-26 22:28:48 -0300

    correction the promisse method

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
 
 const connection = 'mongodb+srv://admin:admin@cluster0-byjrf.mongodb.net/backend?retryWrites=true';
-mongoose.connect(connection, { useNewUrlParser: true });
+mongoose.connect(connection, { useNewUrlParser: true })
+    .then(() => {
+        console.log
+    })
+    .catch((e) => {
+        console.log(e);
+    });
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
